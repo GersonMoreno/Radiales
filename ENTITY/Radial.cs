28678f2377aa2c3630f39df646b0f5bf26d6bf5b
gerson <gersonmore123@gmail.com>
2021-05-11 22:02:34 -0500

    modificacion

--- ENTITY/Radial.cs
+++ ENTITY/Radial.cs
@@ -9,13 +9,11 @@ namespace ENTITY
         public List<double> Valores { get; set; }
         public double Distancia { get; set; }
         public double Activacion { get; set; }
-        public List<Peso> Pesos { get; set; }
-        public Umbral Umbral { get; set; }
+        
         public Radial()
         {
             Valores = new List<double>();
-            Pesos = new List<Peso>();
-            Umbral = new Umbral();
+            
         }
         
     }
