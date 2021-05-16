3b4be6e5e3822dfefe4068b5dbd53fb4316e2d9c
gerson <gersonmore123@gmail.com>
2021-05-15 22:20:54 -0500

    Algoritmo de entrenamiento

--- _BLL/Service.cs
+++ _BLL/Service.cs
@@ -23,29 +23,63 @@ namespace _BLL
         }
         public void Entrenar()
         {
-            List<double> salidas = new List<double>();
-            double ErrorPatron = 0.0,EG=0.0;
+            List<double> Activaciones = new List<double>();
+            List<double> Pesos = new List<double>();
+            double ErrorPatron = 0.0, EG = 0.0;
             
             for (int i = 0; i < Red.Patrones.Count; i++)
             {
                 for (int r = 0; r < Red.Radiales.Count; r++)
                 {
-                    Red.Radiales[r].Distancia = Red.CalcularDistancia(Red.Patrones[i].Entradas, Red.Radiales[r].Valores);
-                    Red.Radiales[r].Activacion = Activacion.Radial(Red.Radiales[r].Distancia);
-
-                    Red = Repo.Interpolar(Red);
-                }
-                
+                    Red.Radiales[r].Distancia.Add(Red.CalcularDistancia(Red.Patrones[i].Entradas, Red.Radiales[r].Valores));
+                    Red.Radiales[r].Activacion.Add(Activacion.Radial(Red.CalcularDistancia(Red.Patrones[i].Entradas, Red.Radiales[r].Valores)));
+                }   
             }
+            Red = Repo.Interpolar(Red);
+            for (int j = 0; j < Red.Salidas.Count; j++)
+            {
+                for (int i = 0; i < Red.Patrones.Count; i++)
+                {
+                    Activaciones = null;
+                    Pesos = null;
+                    ErrorPatron = 0.0;
+                    for (int h = 0; h < Red.Radiales.Count; h++)
+                    {
+                        Activaciones.Add(Red.Radiales[h].Activacion[i]);
+                        Pesos.Add(Red.Salidas[j].Pesos[h].Valor);
+                    }
+                    
+                    Red.Salidas[j].YR = CalcularYR(Activaciones, Pesos,Red.Salidas[j].Umbral.Valor);
+                    Red.Salidas[j].Error = Red.Salidas[j].YD - Red.Salidas[j].YR;
+                    ErrorPatron += Red.Salidas[j].Error;
+                }
+                ErrorPatron /= Red.Patrones.Count;
+                EG += ErrorPatron;
+            }
+            EG /= Red.Salidas.Count;
+            if (EG> Red.ErrorOptimo)
+            {
+                Entrenar();
+            }
+            else
+            {
+                Console.WriteLine("Criterio de paro");
+            }
+            
         }
-        public double CalcularYR(List<double> salida, List<Peso> Pesos, double Umbral)
+        public double CalcularYR(List<double> Activaciones, List<double> Pesos, double Umbral)
         {
-            return 0.0;
+            double suma = 0.0, Xo = 1.0;
+            for (int i = 0; i < Activaciones.Count; i++)
+            {
+                suma += Activaciones[i] * Pesos[i];
+            }
+            return (Xo * Umbral) + suma;
         }
         public Red GetXML()
         {
             return Repo.GetXML(null);
         }
-
+        
     }
 }
